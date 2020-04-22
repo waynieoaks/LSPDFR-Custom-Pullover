cca8e055b2fb4825954cb89e0800b451bd9a1a14
Waynieoaks <5962977+waynieoaks@users.noreply.github.com>
2020-04-22 19:00:51 +0100

    Add files via upload

---
+++ Custom-Pullover/Main.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rage;
+using Rage.Native;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Reflection;
+
+namespace Custom_Pullover
+{
+
+    using LSPD_First_Response.Mod.API;
+    using Rage;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows.Forms;
+
+    internal class Main : Plugin
+    {
+        public Main()
+        {
+            Game.LogTrivial("Creating Custom Pullover.Main.");
+            Functions.OnOnDutyStateChanged += Functions_OnOnDutyStateChanged;
+            Game.LogTrivial("Done with Custom Pullover.Main.");
+        }
+
+        public override void Finally()
+        {
+            foreach (Vehicle veh in TrafficStopAssist.PlayerVehicles.ToArray())
+            {
+                if (veh.Exists())
+                {
+                    veh.LockStatus = VehicleLockStatus.Unlocked;
+                }
+            }
+        }
+
+        public override void Initialize()
+        {
+            //Event handler for detecting if the player goes on duty
+
+            Game.LogTrivial("Custom Pullover " + Assembly.GetExecutingAssembly().GetName().Version.ToString() + " has been initialised.");
+
+        }
+        
+        internal static string Path = "Plugins/LSPDFR/Custom Pullover.dll";
+
+
+        static void Functions_OnOnDutyStateChanged(bool onDuty)
+        {
+            Game.LogTrivial("Custom Pullover event handler: " + onDuty.ToString());
+            if (onDuty)
+            {
+                    CustomPulloverHandler.Initialise();
+            }
+        }
+    }
+
+    // Wayne added to remove dependency on Albo1125.common
+
+    public static class IniDefaults
+    {
+        public static Keys PositionResetKey = Keys.NumPad5;
+        public static Keys PositionForwardKey = Keys.NumPad8;
+        public static Keys PositionBackwardKey = Keys.NumPad2;
+        public static Keys PositionRightKey = Keys.NumPad6;
+        public static Keys PositionLeftKey = Keys.NumPad4;
+        public static Keys PositionUpKey = Keys.NumPad9;
+        public static Keys PositionDownKey = Keys.NumPad3;
+    }
+
+        public static class ExtensionMethods
+    {
+
+        public static string GetKeyString(Keys MainKey, Keys ModifierKey)
+        {
+            if (ModifierKey == Keys.None)
+            {
+                return MainKey.ToString();
+            }
+            else
+            {
+                string strmodKey = ModifierKey.ToString();
+
+                if (strmodKey.EndsWith("ControlKey") | strmodKey.EndsWith("ShiftKey"))
+                {
+                    strmodKey.Replace("Key", "");
+                }
+
+                if (strmodKey.Contains("ControlKey"))
+                {
+                    strmodKey = "CTRL";
+                }
+                else if (strmodKey.Contains("ShiftKey"))
+                {
+                    strmodKey = "Shift";
+                }
+                else if (strmodKey.Contains("Menu"))
+                {
+                    strmodKey = "ALT";
+                }
+
+                return string.Format("{0} + {1}", strmodKey, MainKey.ToString());
+            }
+        }
+
+        public static bool IsPointOnWater(this Vector3 position)
+        {
+            float height;
+            return NativeFunction.Natives.GET_WATER_HEIGHT<bool>(position.X, position.Y, position.Z, out height);
+        }
+
+        public static bool IsKeyDownComputerCheck(Keys KeyPressed)
+        {
+            if (Rage.Native.NativeFunction.Natives.UPDATE_ONSCREEN_KEYBOARD<int>() != 0)
+            {
+                return Game.IsKeyDown(KeyPressed);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+            public static bool IsKeyDownRightNowComputerCheck(Keys KeyPressed)
+        {
+
+            if (Rage.Native.NativeFunction.Natives.UPDATE_ONSCREEN_KEYBOARD<int>() != 0)
+            {
+                return Game.IsKeyDownRightNow(KeyPressed);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsKeyCombinationDownComputerCheck(Keys MainKey, Keys ModifierKey)
+        {
+            if (MainKey != Keys.None)
+            {
+                return ExtensionMethods.IsKeyDownComputerCheck(MainKey) && (ExtensionMethods.IsKeyDownRightNowComputerCheck(ModifierKey)
+                || (ModifierKey == Keys.None && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.Shift) && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.Control)
+                && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.LControlKey) && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.LShiftKey)));
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsKeyCombinationDownRightNowComputerCheck(Keys MainKey, Keys ModifierKey)
+        {
+            if (MainKey != Keys.None)
+            {
+                return ExtensionMethods.IsKeyDownRightNowComputerCheck(MainKey) && ((ExtensionMethods.IsKeyDownRightNowComputerCheck(ModifierKey)
+                    || (ModifierKey == Keys.None && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.Shift) && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.Control)
+                    && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.LControlKey) && !ExtensionMethods.IsKeyDownRightNowComputerCheck(Keys.LShiftKey))));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Custom-Pullover/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Custom Pullover")]
+[assembly: AssemblyDescription("Custom Pullover script for LSPDFR, based on Traffic Policer, by Albo1125")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Waynieoaks")]
+[assembly: AssemblyProduct("Custom Pullover")]
+[assembly: AssemblyCopyright("Core code Copyright ©  2016-2019 - Albo1125, updates Copyright ©  2019-2020 - Waynieoaks")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ec6b789b-263c-4b73-9768-464ba19d6b18")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
